76ec72d15d9e0c5ee9b31e47db019566839f2837
leoruthenberg <62030394+leoruthenberg@users.noreply.github.com>
2020-03-10 11:07:06 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,7 +5,7 @@ let mouseClicks=0
 let misses = 0
 
 function setup() {
-  createCanvas(displayWidth, displayHeight);
+  createCanvas(windowWidth, windowHeight);
   for (let i = 0; i < 20; i++) {
     bub[i] = new Bubble(random(width), random(height), 20);
   }
