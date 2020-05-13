ca6d973f6f3d691a6a1226d27efeb0c94aed1c22
leoruthenberg <62030394+leoruthenberg@users.noreply.github.com>
2020-05-12 21:55:22 -0700

    Update autopainter.js

--- autopainter.js
+++ autopainter.js
@@ -1,25 +1,43 @@
+function preload() {
+  result = loadStrings('message.txt');
+}
+
 let bruh = []
 let tick = 0
-
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(255)
   strokeWeight(3)
 
-  for (let i = 0; i < 10; i++) {
+canvas = createCanvas(windowWidth, windowHeight);
+  canvas.position(0, 0);
+  canvas.style('z-index', '-1')
+
+  for (let i = 0; i < 40; i++) {
     bruh[i] = new brush();
   }
 }
 
 function draw() {
-  for (let i = 0; i < bruh.length; i++) {
+  // background(255)
+
+  for (let i = 0; i < 40; i++) {
     bruh[i].move();
     bruh[i].show();
   }
   tick += 1
-  if (tick >= 3000) {
+  if (tick >= 2000) {
     print(tick)
     noLoop();
   }
+
+  function mousePressed() {
+    fill(255)
+    ellipse(mouseX, mouseY, 15, 15)
+  }
+}
+function windowResized() {
+  resizeCanvas(windowWidth,windowHeight);
+  
 }
